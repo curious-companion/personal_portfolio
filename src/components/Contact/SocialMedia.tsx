cae829e02d49a45a1a3eefb038378d330338dafa
= <=>
2025-04-28 11:19:35 +0530

    Bug Fixes

--- src/components/Contact/SocialMedia.tsx
+++ src/components/Contact/SocialMedia.tsx
@@ -1,47 +1,50 @@
-import {Box} from "@mui/material"
+import { Box } from "@mui/material";
 import SocialMediaIcon from './SocialMediaIcon';
 import XIcon from '../../../assets/icons/X.svg';
+import LinkedinIcon from '../../../assets/icons/linkedin.svg';
+import GithubIcon from '../../../assets/icons/icons8-github.svg';
 
 export const SocialMediaArray = [
-    {
-        title: 'LinkedIn',
-        href: 'https://www.linkedin.com/in/vibhanshu-jain-23a893225/',
-        color: '#0e76a8',
-        svg: 'https://www.svgrepo.com/show/138936/linkedin.svg'
-
-    }, {
-        color: '#171515',
-        title: 'Github',
-        href: 'https://github.com/curious-companion',
-        svg: 'https://www.svgrepo.com/show/343674/github.svg'
-    }, {
-        title: 'X',
-        href: "https://x.com/Vibhanshujainnn",
-        color: '#4267B2',
-        svg: <XIcon width={24} height={24} />
-    }
-]
+  {
+    title: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/vibhanshu-jain-23a893225/',
+    color: '#0e76a8',
+    svg: <LinkedinIcon width={24} height={24} />
+  },
+  {
+    title: 'Github',
+    href: 'https://github.com/curious-companion',
+    color: '#171515',
+    svg: <GithubIcon width={24} height={24} />
+  },
+  {
+    title: 'X',
+    href: "https://x.com/Vibhanshujainnn",
+    color: '#4267B2',
+    svg: <XIcon width={24} height={24} />
+  }
+];
 
 const SocialMedia = () => {
+  return (
+    <Box sx={{
+      pt: '1em',
+      pb: '3em',
+      display: 'flex',
+      gap: '1em',
+      justifyContent: 'center'
+    }}>
+      {SocialMediaArray.map((icon) => (
+        <SocialMediaIcon
+          svg={icon.svg}
+          key={icon.href}
+          title={icon.title}
+          href={icon.href}
+          color={icon.color}
+        />
+      ))}
+    </Box>
+  );
+};
 
-    return (
-        <Box sx={{
-            pt: '1em',
-            pb:'3em',
-            display: 'flex'
-        }}>
-            {SocialMediaArray.map(icon => {
-
-                return <SocialMediaIcon
-                    svg={icon.svg}
-                    key={icon.href}
-                    title={icon.title}
-                    href={icon.href}
-                    color={icon.color}/>
-            })}
-        </Box>
-
-    )
-}
-
-export default SocialMedia
+export default SocialMedia;
--- src/components/Contact/SocialMediaIcon.tsx
+++ src/components/Contact/SocialMediaIcon.tsx
@@ -1,64 +1,56 @@
-import { Box, Typography } from "@mui/material"
-import { centeredStyles } from "../Sections/Perks/Perks"
-import Image from 'next/image'
-import { ISocialMedia } from "../../Types/Types"
-import { isValidElement } from "react"
+import { Box, Typography } from "@mui/material";
+import { centeredStyles } from "../Sections/Perks/Perks";
+import Image from "next/image";
+import { ISocialMedia } from "../../Types/Types";
+import { isValidElement } from "react";
 
-const SocialMediaIcon = ({title, svg, href, filter} : ISocialMedia) => {
-    return (
-        <Box
-            sx={{
-            my: '.5em',
-            maxWidth: '150px',
-            transition: '.2s ease',
-            ...centeredStyles,
-            width: {
-                xs: '50%',
-                sm: '25%'
-            },
-            margin: '0 auto',
-            
-        }}>
-            <a target="_blank" rel="noreferrer" href={href}>
+const SocialMediaIcon = ({ title, svg, href, filter }: ISocialMedia) => {
+  return (
+    <Box
+      component="a"
+      target="_blank"
+      rel="noreferrer"
+      href={href}
+      sx={{
+        my: ".5em",
+        maxWidth: "150px",
+        transition: ".2s ease",
+        ...centeredStyles,
+        width: { xs: "50%", sm: "25%" },
+        margin: "0 auto",
+        textDecoration: "none", // important
+      }}
+    >
+      <Box
+        sx={{
+          ...centeredStyles,
+          width: { xs: "35px" },
+          height: { xs: "35px" },
+        }}
+      >
+        {isValidElement(svg) ? (
+          svg
+        ) : (
+          <Image
+            alt="Icon"
+            className={`${filter ? "filter" : ""} icon`}
+            width={24}
+            height={24}
+            src={svg as string}
+          />
+        )}
+      </Box>
+      <Typography
+        variant="h3"
+        sx={{
+          pt: ".25em",
+          fontSize: { xs: ".75em" },
+        }}
+      >
+        {title}
+      </Typography>
+    </Box>
+  );
+};
 
-                <Box
-                    sx={{
-                    ...centeredStyles,
-                    width: {
-                        xs: '35px'
-                    },
-                    height: {
-                        xs: '35px'
-                    },
-                  
-                }}>
-
-                    {typeof svg === 'string' ?(
-                        <Image
-                            alt='Icon'
-                            className={`${filter
-                            ? 'filter '
-                            : ''} icon`}
-                            width={24}
-                            height={24}
-                            src={svg}
-                        />
-                    ):(
-                        isValidElement(svg) && svg
-                    )}
-                </Box>
-                <Typography
-                    variant='h3'
-                    sx={{
-                    pt: '.25em',
-                    fontSize: {
-                        xs: '.75em'
-                    }
-                }}>{title}</Typography>
-            </a>
-
-        </Box>
-    )
-}
-
-export default SocialMediaIcon
+export default SocialMediaIcon;
