d9faaee0ac6509b3e1738b3fde8b40ef3d2080cd
= <=>
2025-05-24 19:31:26 +0530

    Google verification added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 const Document = () => (
   <Html>
     <Head>
+      <meta name="google-site-verification" content="google0785746e60efc728" />
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href={`https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;700&display=swap`} rel="stylesheet" />
